89ae31de5149bf3fedc09f5b36a1e2bf67e2eb47
mergehez <argesozdogan@gmail.com>
2026-05-11 17:24:52 +0200

    move back to plain 'vite' from 'vite-plus'

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+/// <reference types="vitest/config" />
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
-import { defineConfig } from 'vite-plus';
+import { defineConfig } from 'vite';
 import electronApiMethods from './vite-export-api-methods.ts';
 
 export default defineConfig({
